4a2d53dffe22cf39da4b3c09aff65ed3f3092f9b
Madgalena Rahn <magdalena.m.rahn@gmail.com>
2023-02-10 16:22:06 -0600

    updated project notebook

--- acquire_wrangle.py
+++ acquire_wrangle.py
@@ -3,6 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from sklearn.metrics import mean_squared_error
+from math import sqrt 
+
 from env import username, host, password
 
 
@@ -18,7 +21,22 @@ def get_temp_data():
                        
 
         
-        
+def evaluate(target_var):
+    
+    '''
+    This function will take the actual values of the target_var from validate, 
+    and the predicted values stored in yhat_df, 
+    and compute the rmse, rounding to 0 decimal places. 
+    it will return the rmse. 
+    '''
+    
+    rmse = round(sqrt(mean_squared_error(val[target_var], yhat_df[target_var])), 0)
+    print(f'The RMSE on train is {rmse}.')
+    
+    return rmse
+
+
+
         
         
 # datetime temp clean-up function
@@ -56,3 +74,38 @@ def temp_time(df):
     return df        
         
         
+
+def append_eval_df(model_type, target_var):
+    
+    rmse = evaluate(target_var)
+    
+    f = {'model_type' : [model_type], 'target_var' : [target_var], 'RMSE' : [rmse]}
+    
+    f = pd.DataFrame(f)
+    
+    return eval_df.append(f, ignore_index = True)
+        
+        
+        
+# plotting and RMSE function
+
+def plot_eval(train, val, yhat_df, target):
+    
+    '''
+    This function takes in the target variable and returns a graph
+    of the train, validate and predicted values from the yhat dataframe.
+    It also labels the RMSE.
+    '''
+    
+    plt.figure(figsize = (12, 6))
+    
+    plt.plot(train[target], label = 'Train', linewidth = 0.7, color = 'm')
+    plt.plot(val[target], label = 'Validate', linewidth = 0.7, color = 'y')
+    plt.plot(yhat_df[target], label = 'yhat', linewidth = 0.9, c = 'g')
+    
+    plt.legend()
+    plt.title('Average Temperature')
+
+    
+    plt.show()
+            
